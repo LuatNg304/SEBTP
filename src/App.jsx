2dfcd3a321d473dde0068f210f0a0d8e00b7d446
luatluat304@gmail.com <luatluat304@gmail.com>
2025-09-26 00:40:52 +0700

    pageHome

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import CategoryGrid from "./components/header/categoryGrid";
 import Navbar from "./components/header/navbar";
 import Banner from "./components/header/banner";
 import ItemCard from "./components/body/item";
+import HomePage from "./pages/home";
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar/>
+      {/* <Navbar/>
       <Banner/>
       <CategoryGrid/>
-      <ItemCard/>
+      <ItemCard/> */}
+      <HomePage/>
     </div>
   );
 }
