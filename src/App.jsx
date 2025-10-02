403674ce24d1ca240899112bd22d146fcaa9450a
luatluat304@gmail.com <luatluat304@gmail.com>
2025-10-02 16:53:52 +0700

    update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import HomePage from "./pages/home";
-import { BrowserRouter as Router, Routes, Route, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
 import ForgotPasswordPage from "./pages/forgotPass/ForgotPasswordPage";
 import OTPPage from "./pages/forgotPass/Otp";
 import ResetPasswordPage from "./pages/forgotPass/resetPass";
+import { ToastContainer } from "react-toastify";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <HomePage />,
     },
+    {
+      path: "/forgot-password",
+      element: <ForgotPasswordPage />,
+    },
+    {
+      path: "/otp",
+      element: <OTPPage />,
+    },
+    {
+      path: "/reset-password",
+      element: <ResetPasswordPage />,
+
+    }
   ]);
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/otp" element={<OTPPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-      </Routes>
-    </Router>
+    <>
+      <RouterProvider router={router} />;
+      <ToastContainer />
+    </>
   );
 }
 
